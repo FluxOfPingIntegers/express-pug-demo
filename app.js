f8341f7f781cf369e5feddf3d6dd51d8c4e47ce2
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-07-02 11:10:42 -0400

    adding comments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require ('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// this variable is a valid request handler
 const app = express();
 
 // telling express to use the pug templating engine
@@ -11,15 +12,19 @@ app.set('view engine', 'pug');
 // telling express where to find our templates.  This IS views by default
 app.set('views', 'views');
 
+// importing our pug middleware
 const pugRoutes = require('./routes/pug');
 
+// allowing user submitted data to be available in the request object
 app.use(bodyParser.urlencoded({extended: false}));
 
 // allows our html files to search within the public folder for links & scripts
 app.use(express.static(path.join(__dirname, 'public')));
 
+// using our pug middleware functions to handle http requests beginning with '/pugs'
 app.use('/pugs', pugRoutes);
 
+// this middleware function wil catch all non-defined routes
 app.use((req, res, next) => {
   res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
--- routes/pug.js
+++ routes/pug.js
@@ -4,8 +4,10 @@ const express = require('express');
 
 const router = express.Router();
 
+// this is our array of pugs that'll be held in memory
 const pugs = [];
 
+// /pugs/new => GET
 router.get('/new', (req, res, next) => {
   res.render('pugs-new', {
     pageTitle: 'Create Pug',
@@ -13,22 +15,29 @@ router.get('/new', (req, res, next) => {
   });
 });
 
+
+// /pugs/create => POST
 router.post('/create', (req, res, next) => {
+  // creating object which is our submitted form data in req.body
   const pug = {
     "name": req.body.name,
     "age": req.body.age,
     "owner": req.body.owner
   };
+  // adding object to list in memory
   pugs.push(pug);
   console.log("*SNORT* bow-wow!", pug);
   res.redirect('/pugs');
 });
 
+// /pugs => GET
 router.get('/', (req, res, next) => {
+    // passing object as 2nd argument with a key of 'pugs' holding our list
     res.render('pugs-index', {
       pageTitle: 'Pug List',
       pugs: pugs
     });
 })
 
+// exports our middleware
 module.exports = router;
